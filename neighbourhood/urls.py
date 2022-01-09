6a3c74f4e172fdedc648a9e7f3795263f004bd69
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-01-09 12:21:26 +0300

    accounts

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -20,6 +20,7 @@ from django.contrib.auth import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('neighbour.urls')),
+    url(r'^accounts/',include('registration.backends.simple.urls')),
     
     
 ]
