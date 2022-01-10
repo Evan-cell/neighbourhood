7094ea058feefa431e9e33aaf5b6d74f44d85909
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-01-10 05:48:46 +0300

    authorities view

--- neighbour/views.py
+++ neighbour/views.py
@@ -48,4 +48,11 @@ def health(request):
     profile=Profile.objects.get(username=current_user)
     healthservices = Health.objects.filter(neighbourhood=profile.neighbourhood)
 
-    return render(request,'health.html',{"healthservices":healthservices})    
+    return render(request,'health.html',{"healthservices":healthservices})
+@login_required(login_url='/accounts/login/')
+def authorities(request):
+    current_user=request.user
+    profile=Profile.objects.get(username=current_user)
+    authorities = Authorities.objects.filter(neighbourhood=profile.neighbourhood)
+
+    return render(request,'authorities.html',{"authorities":authorities})
