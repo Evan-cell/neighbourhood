d5e0fcafe642914ff0aed5261ab8b70aa2cb8c49
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-01-10 05:53:51 +0300

    create_profile

--- neighbour/views.py
+++ neighbour/views.py
@@ -135,3 +135,18 @@ def new_business(request):
         form = BusinessForm()
 
     return render(request,'business_form.html',{"form":form})
+@login_required(login_url='/accounts/login/')
+def create_profile(request):
+    current_user=request.user
+    if request.method=="POST":
+        form =ProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            profile = form.save(commit = False)
+            profile.username = current_user
+            profile.save()
+        return HttpResponseRedirect('/')
+
+    else:
+
+        form = ProfileForm()
+    return render(request,'profile_form.html',{"form":form})
